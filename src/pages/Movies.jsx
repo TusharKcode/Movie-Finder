d0e4200bd76cb4370eb92a6389db0dfc5b6f0065
Tushar Kumar <tkgoyal104@gmail.com>
2025-09-05 12:52:17 +0530

    Files Updated

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,7 +1,36 @@
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import CardActionArea from '@mui/material/CardActionArea';
+
 function Movies() {
   return (
     <>
-    <h1>This is Movie Section</h1>
+    <div style={{
+          padding:"40px",
+          backgroundColor:"",
+               
+        }}>
+          <Card sx={{ maxWidth: 345 }}>
+            <CardActionArea>
+              <CardMedia style={{height:"300px", objectFit:"cover"}}
+                component="img"
+                height="25"
+                image="{image}"
+                alt="{imgText}"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div" sx={{color:"#450787ff", fontWeight:"bold", fontSize:"25px", fontStretch:"expanded"}}>
+                  name
+                </Typography>
+                <Typography variant="body2" sx={{ color: 'text.secondary', fontStyle:"oblique", fontSize:"15px"}}>
+                  "description"
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+          </Card>
+        </div>
     </>
   );
 }
--- src/pages/People.jsx
+++ src/pages/People.jsx
@@ -5,64 +5,28 @@ import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import CardActionArea from '@mui/material/CardActionArea';
 
-// export default function PopularPeopleCard({name, description, image, imgText} ) {
-//   return (
-    
-//     <div style={{
-//       padding:"40px",
-//       backgroundColor:"",
-           
-//     }}>
-//       <Card sx={{ maxWidth: 345 }}>
-//         <CardActionArea>
-//           <CardMedia style={{height:"300px", objectFit:"cover"}}
-//             component="img"
-//             height="25"
-//             image={image}
-//             alt={imgText}
-//           />
-//           <CardContent>
-//             <Typography gutterBottom variant="h5" component="div" sx={{color:"#450787ff", fontWeight:"bold", fontSize:"25px", fontStretch:"expanded"}}>
-//               {name}
-//             </Typography>
-//             <Typography variant="body2" sx={{ color: 'text.secondary', fontStyle:"oblique", fontSize:"15px"}}>
-//               {description}
-//             </Typography>
-//           </CardContent>
-//         </CardActionArea>
-//       </Card>
-//     </div>
-//   );
-// }
-
-export default function PopularPeopleCard() {
-
-
-  // See details of People
-  const onSeeInfo = () =>{
-    console.log("Actors description");
-  };
-
+export default function PopularPeopleCard({name, description, image, imgText} ) {
   return (
+    
     <div style={{
       padding:"40px",
       backgroundColor:"",
            
     }}>
-      <Card sx={{ maxWidth: 345 }} onClick={onSeeInfo}>
+      <Card sx={{ maxWidth: 345 }}>
         <CardActionArea>
           <CardMedia style={{height:"300px", objectFit:"cover"}}
             component="img"
             height="25"
-            image="/src/assets/images (1).jpeg"
-            alt="Robert Downey Jr."
+            image={image}
+            alt={imgText}
           />
           <CardContent>
             <Typography gutterBottom variant="h5" component="div" sx={{color:"#450787ff", fontWeight:"bold", fontSize:"25px", fontStretch:"expanded"}}>
-              Robert Downey Jr.
+              {name}
             </Typography>
             <Typography variant="body2" sx={{ color: 'text.secondary', fontStyle:"oblique", fontSize:"15px"}}>
-              Iron Man, Avengers, Sherlock Holmes
+              {description}
             </Typography>
           </CardContent>
         </CardActionArea>
@@ -70,3 +34,39 @@ export default function PopularPeopleCard() {
     </div>
   );
 }
+
+// export default function PopularPeopleCard() {
+
+
+//   // See details of People
+//   const onSeeInfo = () =>{
+//     console.log("Actors description");
+//   };
+
+//   return (
+//     <div style={{
+//       padding:"40px",
+//       backgroundColor:"",
+           
+//     }}>
+//       <Card sx={{ maxWidth: 345 }} onClick={onSeeInfo}>
+//         <CardActionArea>
+//           <CardMedia style={{height:"300px", objectFit:"cover"}}
+//             component="img"
+//             height="25"
+//             image="/src/assets/images (1).jpeg"
+//             alt="Robert Downey Jr."
+//           />
+//           <CardContent>
+//             <Typography gutterBottom variant="h5" component="div" sx={{color:"#450787ff", fontWeight:"bold", fontSize:"25px", fontStretch:"expanded"}}>
+//               Robert Downey Jr.
+//             </Typography>
+//             <Typography variant="body2" sx={{ color: 'text.secondary', fontStyle:"oblique", fontSize:"15px"}}>
+//               Iron Man, Avengers, Sherlock Holmes
+//             </Typography>
+//           </CardContent>
+//         </CardActionArea>
+//       </Card>
+//     </div>
+//   );
+// }
